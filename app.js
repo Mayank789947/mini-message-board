834c4b890dd6608a4ba5e772d85729e0e86e1bbd
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-07-23 09:38:35 +0530

    Setup basic ejs template engine
    setup ejs template engine to serve frontend view and static assets on app

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const links = [
+    { href: "/", text: "Home" }
+]
+
 app.get("/", (req, res) => {
-    res.end("Hello, World!");
+    res.render("index", { links: links });
 });
 
 app.listen(PORT, () => {
