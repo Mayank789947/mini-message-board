cfd8e5674bef280ffe65b9a603b1bd37a3aa99bd
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-07-23 12:35:49 +0530

    Create new router for every message display dynamically
    create new router that display specific message on new page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -11,6 +11,8 @@ router.get("/new", (req, res) => {
     res.render("form");
 });
 
-router.post("/new", messageController);
+router.post("/new", messageController.createMessage);
+
+router.get("/messages/:id", messageController.getMessageDetails);
 
 module.exports = router;
