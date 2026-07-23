b2eebe7af6cf23704a40500c08f934ce76830414
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-07-23 18:31:30 +0530

    Install and configure environment variables and files
    install environment package and configure it to access port variable from the .env file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("node:path");
 const indexRoutes = require("./routes/indexRouter");
 
-const PORT = 3000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
