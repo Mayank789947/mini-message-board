2ad5076b79533af478edee12dc0ffc47e22a867e
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-07-30 20:18:51 +0530

    Remove test database connection and related logging from pool configuration
    remove test database connection and related logging from pool configuration to back our code in original

--- db/pool.js
+++ db/pool.js
@@ -1,17 +1,10 @@
 const { Pool } = require("pg");
 
-console.log("DB_URL exists:", !!process.env.DB_URL);
-console.log("DB_URL starts with:", process.env.DB_URL?.slice(0, 40));
-
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-pool.on("error", (err) => {
-  console.error("Pool Error:", err);
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
